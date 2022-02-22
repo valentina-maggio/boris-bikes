57dee4cc049c60b8c38b60a9b21cf615ea748421
Valentina <maggio.vale@gmail.com>
2022-02-22 17:28:57 +0000

    Added first unit test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
+  docking_station = DockingStation.new
 
-  
+  it "responds to release_bike method" do
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
