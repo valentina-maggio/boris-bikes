211e6c7c79983c4f949417383941edcc6c29d4fc
Jonny Abrams <jonnyabrams@hotmail.com>
2022-02-23 14:53:13 +0000

    Refactored 'gets bike' test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,11 @@
+require 'bike'
+
 class DockingStation
-  def release_bike
-    "bike"
+  def initialize(bike = Bike.new)
+    @bike = bike
   end
   
+  def release_bike
+    @bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,12 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
+  bike = Bike.new
   it "responds to release_bike method" do
     expect(subject).to respond_to(:release_bike)
   end
   it "gets a bike" do
-    expect(subject.release_bike).to eq "bike"
+    expect(subject.release_bike).to eq bike
   end
 end
